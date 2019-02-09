6592ad3355582619c91525a4bf15df3d5c2dfaee
root <root@ip-172-31-37-180.eu-central-1.compute.internal>
2019-02-09 09:59:01 +0000

    Changed index page contents.

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
-<html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<p>Hello World</p>'; ?> 
- </body>
-</html>
+<?php
+print "Hello World! php version is " . PHP_VERSION . "\n";
+print "A change is a coming!\n";
+?>
